0b27dd478d2739638a9579abdd4aea5ce84c2127
Tantorn Dhippayom <tantornd@gmail.com>
2025-10-01 17:50:28 +0700

    A08

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Banner from '@/components/Banner';
+import PromoteCard from '@/components/PromoteCard';
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Banner />
+      
+      {/* PromoteCard section */}
+      <PromoteCard />
 
       <main className="flex-1 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gradient-to-b from-gray-50 to-white dark:from-[#1a1a1a] dark:to-[#0a0a0a]">
         <section className="text-center">
